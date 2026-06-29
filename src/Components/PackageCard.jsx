b57dca8d3c22806868c8240f60938b4e967cb16f
VASAVA KULDIP <vasavakuldip37@gmail.com>
2026-06-29 17:01:42 +0530

    Add contact form validation using React Hook Form and Zod

--- src/Components/PackageCard.jsx
+++ src/Components/PackageCard.jsx
@@ -1,61 +1,41 @@
 import { Link } from "react-router-dom";
-import {useState} from "react";
+import { useState } from "react";
 
 function PackageCard({ image, title, location, duration, price, rating }) {
   const [isHovered, setIsHovered] = useState(false);
   const [saved, setSaved] = useState(false);
-  const handleWishlist=()=>{
+  const handleWishlist = () => {
     setSaved(!saved);
-  }
+  };
   return (
     <div className="bg-white rounded-2xl overflow-hidden shadow-lg hover:shadow-2xl transition duration-300">
-
-      <img
-        src={image}
-        alt={title}
-        className="w-full h-56 object-cover"
-      />
+      <img src={image} alt={title} className="w-full h-56 object-cover" />
 
       <div className="p-5">
+        <h2 className="text-xl font-bold">{title}</h2>
 
-        <h2 className="text-xl font-bold">
-          {title}
-        </h2>
+        <p className="text-gray-500 mt-2">📍 {location}</p>
 
-        <p className="text-gray-500 mt-2">
-          📍 {location}
-        </p>
-
-        <p className="text-gray-500">
-          ⏳ {duration}
-        </p>
+        <p className="text-gray-500">⏳ {duration}</p>
 
         <div className="flex justify-between items-center mt-4">
+          <span className="text-cyan-600 font-bold text-xl">₹{price}</span>
 
-          <span className="text-cyan-600 font-bold text-xl">
-            ₹{price}
-          </span>
-
-          <span className="text-yellow-500">
-            ⭐ {rating}
-          </span>
-
+          <span className="text-yellow-500">⭐ {rating}</span>
         </div>
         <button
-  onClick={handleWishlist}
-  className="w-full border py-2 rounded-lg mb-3 hover:bg-pink-50"
->
-  {saved ? "❤️ Added To Wishlist" : "🤍 Add To Wishlist"}
-</button>
+          onClick={handleWishlist}
+          className="w-full border py-2 rounded-lg mb-3 hover:bg-pink-50"
+        >
+          {saved ? "❤️ Added To Wishlist" : "🤍 Add To Wishlist"}
+        </button>
         <Link
           to={`/package/${title}`}
           className="block mt-5 text-center bg-cyan-500 text-white py-2 rounded-lg hover:bg-cyan-600"
         >
           View Details
         </Link>
-
       </div>
-
     </div>
   );
 }
