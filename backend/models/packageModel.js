92d141ab9f2c909fe56cb612ef78a7598d02c391
VASAVA KULDIP <vasavakuldip37@gmail.com>
2026-07-04 14:15:57 +0530

    Create package model

---
+++ backend/models/packageModel.js
@@ -0,0 +1,27 @@
+const db=require("../config/db");
+const createPackage=(packageData,callback)=>{
+    const sql="INSERT INTO packages
+    (title,description,location,price,duration,category,image) VAlUES(?,?,?,?,?,?,?)";
+    db.query(sql,
+        [packageData.title,
+            packageData.description,
+            packageData.location,
+            packageData.price,
+            packageData.duration,
+            packageData.category,
+            packageData.image],
+        ,callback);
+
+};
+//get all packages
+const getAllpackages=(callback)=>{
+    const sql="SELECT * FROM packages ORDER BY id DESC";
+    db.query(sql,callback);
+};
+
+//export
+module.exports={
+    createpackage,
+    getAllpackages
+
+};
