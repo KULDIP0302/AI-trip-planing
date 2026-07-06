3dfd03b8470e85b72e87c14a328c83ed64f019f0
VASAVA KULDIP <vasavakuldip37@gmail.com>
2026-07-06 16:10:37 +0530

    Add update package API

--- backend/routes/packageRoutes.js
+++ backend/routes/packageRoutes.js
@@ -6,11 +6,13 @@ const {
   addPackage,
   getAllPackages,
   getPackageById,
+  updatePackage,
 } = require("../controllers/packageController");
 
 //add package
 router.post("/add-package", addPackage);
 router.get("/get-packages", getAllPackages);
 router.get("/get-package/:id", getPackageById);
+router.put("/update-package/:id", updatePackage);
 
 module.exports = router;
